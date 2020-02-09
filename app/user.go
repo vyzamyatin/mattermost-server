c46e203037805684821655cd9e5ffcec84d1f7de
Vyacheslav Zamyatin <vyzamyatin@gmail.com>
2020-02-09 14:52:01 +0300

    [VZ] replace [author name] with surrogate user

--- app/user.go
+++ app/user.go
@@ -452,6 +452,15 @@ func (a *App) GetUserByUsername(username string) (*model.User, *model.AppError) 
 	return result, nil
 }
 
+func (a *App) GetUserByNickname(nickname string) (*model.User, *model.AppError) {
+	result, err := a.Srv.Store.User().GetByNickname(nickname)
+	if err != nil && err.Id == "store.sql_user.get_by_username.app_error" {
+		err.StatusCode = http.StatusNotFound
+		return nil, err
+	}
+	return result, nil
+}
+
 func (a *App) GetUserByEmail(email string) (*model.User, *model.AppError) {
 	user, err := a.Srv.Store.User().GetByEmail(email)
 	if err != nil {
